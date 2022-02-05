f103f431bd022e26ffb6374fbf4ae63d7183e175
Shivaansh1808 <72135426+Shivaansh1808@users.noreply.github.com>
2022-02-05 22:30:06 +0530

    Update uploadFiles.py

--- uploadFiles.py
+++ uploadFiles.py
@@ -10,23 +10,23 @@ class TransferData():
         dbx = dropbox.Dropbox(self.access_token)
 
         with open(file_from, "rb") as f:
-            dbx.file.upload(f.read(), file_to)
+            dbx.files_upload(f.read(), file_to)
 
 #not able to do it :(
 """        for root, dirs, files in os.walk(file_from):
-
             for filename in files:
                     # making full local path
                 local_path = os.path.join(root, filename)
                     # making full Dropbox path
                 relative_path = os.path.relpath(local_path, file_from)
                 dropbox_path = os.path.join(file_to, relative_path)
+
                     # upload the file
                 with open(local_path, 'rb') as f:
                     dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))"""
 
 def main():
-    access_token = "dHEqFgWNOVQAAAAAAAAAAY3q--9zlJfmK3G0DWfAdrdh4rjpQaip5Py49WKFNdJo"
+    access_token = "J8WuQYvzEe0AAAAAAAAAAd7KwbpMTUmmrRjqAouhCuNanS58jTkpeS_ElV-64VhM"
     transferData = TransferData(access_token)
 
     file_from = input("Enter the file path to transfer: ")
